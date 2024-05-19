47f4b1422ccf69e8a1254bf62cd69d21d5d572f2
kashish1203 <202001425@daiict.ac.in>
2024-05-20 03:54:20 +0530

    App

--- MauiApp4/MainPage.xaml.cs
+++ MauiApp4/MainPage.xaml.cs
@@ -1,25 +1,15 @@
-﻿namespace MauiApp4
+﻿namespace MauiApp4;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+
+    public MainPage()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        InitializeComponent();
     }
 
+    private void ControlBtn_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new ControlDemo());
+    }
 }
