3965310b936c24bec6ff09df654f10aaa604511c
kashish1203 <202001425@daiict.ac.in>
2024-05-20 03:01:17 +0530

    App3

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void SLayoutBtn_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new StackDemo());
+    }
+
+    private void SLayoutBtn_Clicked_1(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new HStackDemo());
+    }
+
+    private void GridBtn_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new GridDemo());
+    }
+
+    private void OnCalc_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new CalcDemo());
+    }
+
+    private void FlexBtn_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new FlexDemo());
+    }
+
+    private void OnAbsoluteClicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new AbsoluteDemo());
+    }
+
+}
